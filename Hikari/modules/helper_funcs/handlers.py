0e2eaebcf2380e6321d0d064509e2b95287cc444
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-09-07 04:22:55 +0700

    Update handlers.py

--- Hikari/modules/helper_funcs/handlers.py
+++ Hikari/modules/helper_funcs/handlers.py
@@ -43,6 +43,20 @@ class AntiSpam:
             + (TIGERS or [])
         )
 
+    # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
+        Duration.CUSTOM = 6  # Custom duration, 20 seconds
+        self.sec_limit = Rate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
+        self.min_limit = Rate(20, Duration.MINUTE)  # 20 / Per minute
+        self.hour_limit = Rate(100, Duration.HOUR)  # 100 / Per hour
+        self.daily_limit = Rate(1000, Duration.DAY)  # 1000 / Per day
+        self.limiter = Limiter(
+            self.sec_limit,
+            self.min_limit,
+            self.hour_limit,
+            self.daily_limit,
+            bucket_class=MemoryListBucket,
+        )
+
     def check_user(self, user):
         """
         Return True if user is to be ignored else False
