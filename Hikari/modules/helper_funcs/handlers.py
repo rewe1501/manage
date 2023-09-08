0b4c0e527d084caff82192b5297d4341ae0b003b
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-09-08 08:48:22 +0700

    Update handlers.py

--- Hikari/modules/helper_funcs/handlers.py
+++ Hikari/modules/helper_funcs/handlers.py
@@ -43,7 +43,7 @@ class AntiSpam:
             self.min_limit,
             self.hour_limit,
             self.daily_limit,
-            basic_bucket=InMemoryBucket(Rate)
+            basic_bucket=InMemoryBucket,
         )
 
     def check_user(self, user):
