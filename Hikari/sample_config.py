2c52be2c8e181ae7529f3f26809323ab00d7ef94
rewe1501 <168255947+rewe1501@users.noreply.github.com>
2024-09-04 15:38:27 +0700

    Update sample_config.py

--- Hikari/sample_config.py
+++ Hikari/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "JustRex"
-    SUPPORT_CHAT = "rexaprivateroom"  # Your own group for support, do not add the @
+    OWNER_ID = 1760903792  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "rewetzy"
+    SUPPORT_CHAT = "supprotrewe"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1002237876634
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1002158374397
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
